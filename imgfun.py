df3712d55b0332b4951e55e13eb2f10e6cf4371f
peterliu <peterjliu@gmail.com>
2015-06-13 23:01:04 -0700

    deleted prints. added example ipython notebook

--- imgfun.py
+++ imgfun.py
@@ -23,11 +23,8 @@ def AssignMeans(means, pmat):
     N = pmat.shape[0]
     kdists = np.zeros((K, N))
     for i in range(0, K):
-        print "computing distances to %dth mean" % i
         kdists[i, :] = np.sqrt(np.sum(np.square(np.tile(means[i, :], (N,1)) - pmat), axis=1))
-    print "Compute argmin"
     a = np.argmin(kdists, axis=0)
-    print a.shape
     return a
 
 def ComputeMeans(clusters, pmat, K):
@@ -40,16 +37,11 @@ def ComputeMeans(clusters, pmat, K):
         
     Returns:
         K cluster centers, K x d, where i's center is in row i-1"""
-    print "Start computing means"
     [N, d] = pmat.shape
     means = np.zeros((K, d))
     for i in range(0, K):
-        print "Computing mean %d" % i
-        print clusters.shape
         members = np.reshape(clusters == i, (N, 1))
-        print members.shape
         num_points = np.sum(members)
-        print num_points
         if num_points > 0:
             means[i, :] = np.sum(np.multiply(np.tile(members, (1, d)),
                                  pmat), axis=0) / num_points
@@ -68,25 +60,17 @@ def Kmeans(pmat, K, mini, maxi):
         means: K x d numpy array representing the K means
         clusters: N x 1, cluster assignments, indexed from 0 to K-1"""
     means = np.random.rand(K, 3) * (maxi - mini) + mini
-    print pmat.shape
     [N, d] = pmat.shape
     clusters = np.zeros((N))
     prev_clusters = np.zeros((N))
     iterations = 0
     while True:
         iterations += 1
-        print "Running iteration %d" % iterations
         clusters = AssignMeans(means, pmat)
-        print clusters
-        print clusters.shape
-        print prev_clusters.shape
         agreements = np.sum(np.equal(prev_clusters, clusters))
-        print agreements
         if agreements == N:
             # This ends because K-means is guaranteed to converge
-            print "Converged"
             break
-        print "before compute means"
         means = ComputeMeans(clusters, pmat, K)
         prev_clusters = np.copy(clusters)
     return [means, clusters]
